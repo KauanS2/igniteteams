55dc6b3649147c83acb480c8d5da0a1c302ea01b
KauanS2 <kauans2dev@gmail.com>
2023-09-07 00:21:40 -0300

    feat-group-card

---
+++ src/components/GroupCard/index.tsx
@@ -0,0 +1,13 @@
+import { TouchableOpacityProps } from 'react-native'
+import { Container, Icon, Title } from "@components/GroupCard/styles";
+interface GroupCardProps extends TouchableOpacityProps {
+    title: string
+}
+export function GroupCard({title, ...rest}: GroupCardProps) {
+    return (
+        <Container {...rest}>
+            <Icon />
+            <Title>{title}</Title>
+        </Container>
+    )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,15 @@
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
       <Container>
         <Header />
-        <Highlight title="Turmas" subtitle="jogue com a sua turma"/>
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      
+      <GroupCard title="Nome da turma" />
       </Container>
   );
 }
